f4c997a13208498739d9cde4d2f304e7838da0a5
Shivam Malhotra <malhotrashivam77@gmail.com>
2023-06-25 13:35:31 +0530

    changes

--- jsfcode.js
+++ jsfcode.js
@@ -18,3 +18,22 @@
         document.body.scrollTop=0;
         document.documentElement.scrollTop=0
       }
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwoNTH9ThHCPKxZIwID5MabLMYgq7Sbs4-Pi6Zh5tP-Y_gwlOuuvqiqeh4GvN2YDBFLfA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Thankyou for your message😊"
+        setTimeout(function(){
+          msg.innerHTML=""
+        },3000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
